5ec10eb1d990f3d4248d9b46bb40e75f5efa94ce
pravin2244-del <pj0940990@gmail.com>
2026-07-29 11:44:34 +0530

    array

---
+++ Practice_core_java2/src/Practice_with_java/Array.java
@@ -0,0 +1,18 @@
+package Practice_with_java;
+
+public class Array {
+	public static void main(String[] args)
+	{
+		double arr[] = new double[4];
+		arr[0]=3;
+		arr[1]=4;
+		arr[2]=5;
+		arr[3]=8;
+		
+		System.out.println(arr[0]);
+		System.out.println(arr[0]+arr[1]+"winterfail is in north"+arr[2]+arr[3]);
+		System.out.println(arr[1]%2);
+		System.out.println(arr[3]/2);
+	}
+
+}

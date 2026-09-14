5e2041dee342ea2c2005facfc211d4361885b483
pravin2244-del <pj0940990@gmail.com>
2026-09-14 20:10:29 +0530

    firstinterface

---
+++ Practice_core_java2/src/Collection/ArrayLists.java
@@ -0,0 +1,22 @@
+package Collection;
+
+import java.util.ArrayList;
+
+public class ArrayLists {
+	
+	public static void main(String[] args)
+	{
+		ArrayList<String> a = new ArrayList<String>();
+		
+		a.add("Hey");
+		a.add("pravin");
+		a.add("how are you");
+		
+		System.out.println(a);
+		
+		
+	}
+
+	
+	 
+}
